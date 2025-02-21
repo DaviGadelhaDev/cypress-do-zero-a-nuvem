43589260b355d0a4d8daf4fad954e35f1acf639d
DavigadelhaDev <davigadelhadev@gmail.com>
2025-02-21 15:43:05 -0300

    Finished exercise 6

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,77 +1,84 @@
-// 🔹 Funções auxiliares globais (acessíveis a todos os testes)
 function stringGen(length) {
   return "A".repeat(length);
 }
 
-function preencheCamposObrigatorios() {
+function fillRequiredFields() {
   cy.get('#firstName').type('Davi');
   cy.get('#lastName').type('Gadelha');
   cy.get('#email').type('davibrgadelha@gmail.com');
   cy.get('textarea').type('testando textarea', { delay: 0 });
 }
 
-function campos_array() {
+function getFieldSelectors() {
   return ['#firstName', '#lastName', '#email', '#phone'];
 }
 
-describe('Central de Atendimento ao Cliente', () => {
+describe('Central of support to Client', () => {
   beforeEach(() => {
     cy.visit('./src/index.html');
   });
 
-  it('Verifica título', () => {
+  it('Check title', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
   });
 
-  it('preenche os campos obrigatórios e envia o formulário', () => { 
-    preencheCamposObrigatorios();
+  it('Fills the required fields and submits the form', () => { 
+    fillRequiredFields();
     cy.get('button[type="submit"]').click();
     cy.get('.success').should('be.visible');
   });
 
-  it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+  it('Displays an error message when submitting the form with an invalid email format', () => {
     cy.get('#email').type('davigadelha');
     cy.get('button[type="submit"]').click();
     cy.get('.error').should('be.visible');
   });
 
-  it('Preenche e limpa os campos nome, sobrenome, email e telefone' ,() => {
-    const campos = campos_array()
-    const valores = ['Davi', 'Gadelha', 'davibrgadelha@gmail.com', '41984980238']
+  it('Fills and clears the name, email and phone fields' ,() => {
+    const field = getFieldSelectors()
+    const values = ['Davi', 'Gadelha', 'davibrgadelha@gmail.com', '41984980238']
 
-    campos.forEach((seletor, index) => {
-      cy.get(seletor).type(valores[index]).should('have.value', valores[index]).clear().should('have.value', '')
+    field.forEach((selector, index) => {
+      cy.get(selector)
+      .type(values[index])
+      .should('have.value', values[index])
+      .clear()
+      .should('have.value', '')
     })
   });
 
-  describe('Validação do campo de telefone', () => {
-    // 🔹 Função auxiliar específica para este describe
+  it('Displays an error message when submitting an empty form', () => {
+    cy.get('button[type="submit"]').click()
+    cy.get('.error').should('be.visible')
+  })
+
+  context('Check phone field', () => {
     function getPhone() {
       return cy.get('#phone');
     }
 
-    it('Deve ficar vazio ao inserir letras', () => {
+    it('Should clear the phone field when non-numeric input (letters) is entered', () => {
       getPhone().type('abcdef').should('have.value', '');
     });
 
-    it('deve ficar vazio ao inserir caracteres especiais', () => {
+    it('Should clear the phone field when special characters are entered', () => {
       getPhone().type('@#%&*').should('have.value', '');
     });
 
-    it('Deve ficar vazio ao inserir um valor misto', () => {
+    it('Should contain only numeric characters when a mixed value is entered', () => {
       getPhone().type('123ab@456').should('have.value', '123456');
     });
 
-    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
-      preencheCamposObrigatorios();
-      cy.get('#phone-checkbox').check().should('be.checked');
-      cy.wait(100);
-      getPhone().should('have.value', '');
-      cy.get('button[type="submit"]').click();
-      cy.get('.error').should('be.visible');
+    it('Displays an error message when the phone becomes required but isn’t filled before submitting the form', () => {
+      fillRequiredFields(); 
+      cy.get('#phone-checkbox').check().should('be.checked'); 
+      cy.wait(100); 
+      getPhone().should('have.value', ''); 
+      cy.get('button[type="submit"]').click(); 
+      cy.get('.error').should('be.visible'); 
     });
 
-    it('Deve aceitar normalmente', () => {
+    it('Should accept normally', () => {
       getPhone().type('123456').should('have.value', '123456');
     });
   });
