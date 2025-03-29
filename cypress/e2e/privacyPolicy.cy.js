b1476d519e111906d341a39ad4ad8bd611e2b206
DavigadelhaDev <davigadelhadev@gmail.com>
2025-03-28 23:15:46 -0300

    Update documentation

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,5 +1,5 @@
 it.only('independently tests the privacy policy page', () => {
     cy.visit('./src/privacy.html');
     cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible');
-    cy.contains('p', 'Talking About Testing').should('be.visible')
+    cy.contains('p', 'Talking About Testing').should('be.visible');
 })
